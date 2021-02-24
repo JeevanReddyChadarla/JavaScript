5c8e868d07a81bc561794b3ea424f71d330b9473
jeevan <jeevanchadarla1998@gmail.com>
2021-02-24 16:48:23 +0530

    for in loop

---
+++ 8forloop.js
@@ -0,0 +1,44 @@
+var names = ['Jeevan','India','Cognizant','Hyderabad','Asia','Earth','World'];
+
+var len = names.length;
+
+// initialize,test, increase
+
+var i;
+
+// for(i=0;i<len;i++){
+//     console.log(i+" " +names[i]);
+// }
+
+// for(i=0;i<len;i++){
+//     if(i%2==0){
+//         console.log(i+" "+names[i]);
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for (i=len-1;i>=0;i--){
+//     console.log(i+" "+names[i]);
+// }
+
+//** Objects for loop
+
+var newobj = {"name":"Jeevan", "place":"Tirupati", "age": 23};
+console.log(newobj);
+
+//there is no property length in objects
+
+let j;
+for(j in newobj){
+    console.log(j);
+    console.log(newobj[j]);
+}
+
+var newarrobj = ['maths','english',{"science":'physics','nature':'biology',"social":"history"}];
+let k;
+for(k in newarrobj){
+    console.log(k);
+    console.log(newarrobj[k]);
+}
