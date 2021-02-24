e20ea6119ae8bd0971979ecb052ee5629a542304
jeevan <jeevanchadarla1998@gmail.com>
2021-02-24 18:51:13 +0530

    examples

---
+++ Examples/countLeg.js
@@ -0,0 +1,8 @@
+function count(anm1,anm2,anm3){
+    var total = anm1*2 + anm2*4 + anm3*4
+    console.log(total);
+}
+
+count(2,3,5);
+count(1,2,3);
+count(5,2,8);
---
+++ Examples/fillArr.js
@@ -0,0 +1,10 @@
+function num(x){
+    var i;
+    var arr=[];
+    for(i=0;i<x;i++){
+        arr.push(i)
+    }
+    console.log(arr);
+}
+
+num(15);
