64d6a02af707cf329c9cc8d70856e0114c55dfa7
jeevan <jeevanchadarla1998@gmail.com>
2021-02-24 18:41:20 +0530

    examples

---
+++ Examples/BasketBall.js
@@ -0,0 +1,8 @@
+function points(twop, threep){
+    var score =0;
+    score =  twop*2 + threep*3
+    console.log(score);
+}
+
+points(1,1);
+points(7,5);
---
+++ Examples/celTofarn.js
@@ -0,0 +1,13 @@
+function ctoF(temp){
+    var tf;
+    tf=(temp-32)*5/9;
+    console.log(tf);
+}
+ctoF(41);
+
+function fToc(deg){
+    var tempc;
+    tempc = (deg*9/5) + 32;
+    console.log(tempc);
+}
+fToc(5)
